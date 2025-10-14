504b69bda9a0f003a528aa7f60f88c9567ba529f
Evgeniy <jekaolhovskii@gmail.com>
2025-10-14 17:07:26 +0300

    Init TypeScript project with simple example code

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let fristName = "Євгеній";
+let age = 20;
+let isStudent = true;
+console.log(`Мене звати ${fristName}, мені ${age} років. Студент: ${isStudent}`);
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let fristName: string = "Євгеній";
+let age: number = 20;
+let isStudent: boolean = true;
+
+console.log(`Мене звати ${fristName}, мені ${age} років. Студент: ${isStudent}`);
